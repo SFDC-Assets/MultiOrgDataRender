4e455610e55fbad3ed6575ce3815301ce526c0e6
zhou059 <zhou059@gmail.com>
2023-01-11 14:20:00 -0800

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix platform -o multiOrgDataRender.demo
 sfdx force:source:push
-sfdx shane:user:password:set -g User -l User -p salesforce2
-sfdx force:user:permset:assign -n FSCInsurance
-sfdx force:user:permset:assign -n FinancialServicesCloudExtension
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:data:bulk:upsert -s Account -f data/Accounts.csv -i ID -w 3
 sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent

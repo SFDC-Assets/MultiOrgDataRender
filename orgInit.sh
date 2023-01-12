248045ffadbbb6b5639f873628ec14a53b2713c5
jaydeeptase <jaydeeptase@gmail.com>
2023-01-11 19:18:13 -0600

    Data upsert and setting oppty lwc to fetch from api by default

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,4 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --use
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:data:bulk:upsert -s Account -f data/Accounts.csv -i ID -w 3
-sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent
+sfdx force:org:open -p /lightning/o/Account/list
